1ce8162f55be464eb1436f80311a6e877ed470ed
suitedup1 <146772042+suitedup1@users.noreply.github.com>
2026-08-29 18:10:22 +0100

    save

--- code1/lcd.ino
+++ code1/lcd.ino
@@ -1,34 +1,19 @@
-#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F, 16, 2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
 
 void setup() {
-  Serial.begin(115200);
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();  // Make sure backlight is on
 
-  // Leonardo: wait for serial port to connect
-  while (!Serial) {
-  }
+  // Print a message on both lines of the LCD.
+  lcd.setCursor(2, 0);  //Set cursor to character 2 on line 0
+  lcd.print("Hello world!");
 
-  Serial.println();
-  Serial.println("I2C scanner. Scanning ...");
-  byte count = 0;
-
-  Wire.begin();
-  for (byte i = 8; i < 120; i++) {
-    Wire.beginTransmission(i);
-    if (Wire.endTransmission() == 0) {
-      Serial.print("Found address: ");
-      Serial.print(i, DEC);
-      Serial.print(" (0x");
-      Serial.print(i, HEX);
-      Serial.println(")");
-      count++;
-      delay(1);  // maybe unneeded?
-    }            // end of good response
-  }              // end of for loop
-  Serial.println("Done.");
-  Serial.print("Found ");
-  Serial.print(count, DEC);
-  Serial.println(" device(s).");
-}  // end of setup
+  lcd.setCursor(2, 1);  //Move cursor to character 2 on line 1
+  lcd.print("LCD Tutorial");
+}
 
 void loop() {
 }
